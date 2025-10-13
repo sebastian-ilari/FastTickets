7c7b3c3efc50c09b3b734f3ccc5f68a3ae80ae30
Sebastian Ilari <sebahex@gmail.com>
2025-10-13 13:03:32 +0200

    Bring back asynccontextmanager

--- Python/main.py
+++ Python/main.py
@@ -1,29 +1,22 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
 from .data.setup import create_db_and_tables, seed_data_and_session
 from .data.seed import get_application_data
 from .routers import shows, tickets
 
 
-"""
-This doesn't work with integration tests
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Startup code
     create_db_and_tables()
-    seed_database_with_data(get_application_data())
+    seed_data_and_session(get_application_data())
 
     yield
 
     # Shutdown code
 
 app = FastAPI(lifespan=lifespan)
-"""
-
-create_db_and_tables()
-seed_data_and_session(get_application_data())
-
-app = FastAPI()
 
 app.include_router(shows.router)
 app.include_router(tickets.router)
