52a9b6d356a38a438b58c18587e4bb3d4ca1cd63
Sebastian Ilari <sebahex@gmail.com>
2025-10-17 12:07:20 +0200

    Improve is_valid_uuid helper

--- Python/tests/helpers/test_uuid_validator.py
+++ Python/tests/helpers/test_uuid_validator.py
@@ -6,8 +6,12 @@ from ...tests.helpers.uuid_validator import is_valid_uuid
 
 class TestUUIDValidator(unittest.TestCase):
 
-    def test_is_valid_uuid_invalid_uuid_returns_false(self):
+    def test_is_valid_uuid_string_returns_false(self):
         assert not is_valid_uuid("invalid-uuid-string")
+
+    def test_is_valid_uuid_valid_uuid_object_returns_true(self):
+        assert is_valid_uuid(uuid.uuid1())
+        assert is_valid_uuid(uuid.uuid4())
 
     def test_is_valid_uuid_valid_string_uuid_returns_true(self):
         valid_uuid = str(uuid.uuid4())
--- Python/tests/helpers/uuid_validator.py
+++ Python/tests/helpers/uuid_validator.py
@@ -1,6 +1,9 @@
 import uuid
 
 def is_valid_uuid(uuid_to_test: str, version: int =4) -> bool:
+    if isinstance(uuid_to_test, uuid.UUID):
+        return True
+    
     try:
         uuid.UUID(uuid_to_test, version=version)
     except ValueError:
